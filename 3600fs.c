cad2c16ea3d0e6c72c1a3325b23a9e083666c583
aolgin523 <olgin.a@husky.neu.edu>
2014-10-04 14:08:36 -0400

    vcb and dirent structs made

--- 3600fs.c
+++ 3600fs.c
@@ -38,6 +38,41 @@
 #include "3600fs.h"
 #include "disk.h"
 
+// vcb structure
+typedef struct vcb_s {
+  // a magic number of identity of your disk
+  int magic;
+
+  // description of disk layout
+  int blocksize;
+  int de_start;
+  int de_length;
+  int fat_start;
+  int fat_length;
+  int db_start;
+
+  // meta data for root dir
+  uid_t user;
+  gid_t group;
+  mode_t mode;
+  struct timespec access_time;
+  struct timespec modify_time;
+  struct timespec create_time;
+} vcb;
+
+typedef struct dirent_s {
+  unsigned int valid;
+  unsigned int first_block;
+  unsigned int size;
+  uid_t user;
+  gid_t group;
+  mode_t mode;
+  struct timespec access_time;
+  struct timespec modify_time;
+  struct timespec create_time;
+  char name[128]; // arbitrary # for now, will need to change later
+} dirent;
+
 /*
  * Initialize filesystem. Read in file system metadata and initialize
  * memory structures. If there are inconsistencies, now would also be
